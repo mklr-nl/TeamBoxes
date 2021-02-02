c336bc8c167ffca099ff187ef650175160987bbb
Daan Pothof <daanpothof@MacBook-Pro-van-Daan.local>
2021-02-02 23:12:42 +0100

    First steps

--- Components/Team/Index.js
+++ Components/Team/Index.js
@@ -1,15 +1,13 @@
 // * Selectors
-let box1 = document.getElementById("boxOne");
-let box2 = document.getElementById("boxTwo");
-let member1 = document.querySelector(".Team__member__info");
-let memberCover = document.querySelector(".boxPopup");
+let boxOne = document.getElementById("boxOne");
+let boxTwo = document.getElementById("boxTwo");
+let popupBoxOne = document.getElementById("BoxOne__popup");
 
 // * Eventlisteners
-box1.addEventListener("click", changeBox);
+boxOne.addEventListener("click", BoxOnePopup);
 
 // * Functions
-function changeBox() {
-  console.log("hello Danny");
-  box2.classList.toggle("hide");
-  memberCover.classList.toggle("hide");
+function BoxOnePopup() {
+  boxTwo.classList.toggle("hide");
+  popupBoxOne.classList.toggle("hide");
 }
